88205c0ee8fbadd79dcbc987ccd8002aeec7cb30
nataliarico <111695754+nataliarico@users.noreply.github.com>
2022-09-19 17:04:24 -0500

    Create 5.trapecio.py

---
+++ Taller4/5.trapecio.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate
+
+#Notas de clase
+def trapecio(self):
+    self.integral = 0.
+        
+    self.integral += np.exp(x^(-2))*(self.y[0] + self.y[-1])
+        
+    self.integral += np.sum( self.y[0:1] )
+        
+    return self.integral*self.h
